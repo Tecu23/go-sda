33a2a7c7c76efa4b43fb552004b08dd14cbfc71b
Tecu23 <andrei.teculescu@gmail.com>
2024-02-18 20:29:25 +0200

    implement basic algorithms

--- array/array.go
+++ array/array.go
@@ -1,4 +1,47 @@
 // Common algorithms implemented on the standard Array Structure
 package array
 
-// TODO: Implement linear and binary search
+func LinearSearch(arr []int, value int) bool {
+	for _, v := range arr {
+		if v == value {
+			return true
+		}
+	}
+	return false
+}
+
+func BinarySearch(arr []int, value int) bool {
+
+	low := 0
+	high := len(arr) - 1
+
+	for low <= high {
+		mid := low + (high-low)/2
+
+		if arr[mid] == value {
+			return true
+		} else if arr[mid] > value {
+			high = mid
+		} else {
+			low = mid + 1
+		}
+	}
+
+	return false
+}
+
+func SimpleSort(arr *[]int) {
+	for i := 0; i < len(*arr)-1; i++ {
+		for j := i + 1; j < len(*arr); j++ {
+			if (*arr)[i] > (*arr)[j] {
+				tmp := (*arr)[i]
+				(*arr)[i] = (*arr)[j]
+				(*arr)[j] = tmp
+			}
+		}
+	}
+}
+
+func QuickSort(arr *[]int) {}
+
+// TODO: Implement QuickSort
--- main.go
+++ main.go
@@ -1,5 +1,16 @@
 package main
 
+import (
+	"fmt"
+
+	"github.com/Tecu23/go-sda/array"
+)
+
 func main() {
 
+	tmp := []int{203, 4, 10, 12, 7, 8}
+
+	array.SimpleSort(&tmp)
+
+	fmt.Println(tmp)
 }
