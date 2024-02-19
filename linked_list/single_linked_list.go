7146cfe73ae0eaf9841d8a16e929a0eef78a8d45
Tecu23 <andrei.teculescu@gmail.com>
2024-02-19 09:39:29 +0200

    add linked list folder

---
+++ linked_list/single_linked_list.go
@@ -0,0 +1,21 @@
+package linked_list
+
+type Node<T> struct {
+	value T
+	next *Node<T>
+}
+
+
+func Add(node *Node<T>) {}
+
+func AddAtIndex(node *Node<T>, idx number) {}
+
+func AddAtStart(node *Node<T>) {}
+
+func Get(node *Node<T>) {}
+
+func remove() {}
+
+func removeAt(idx number) {}
+
+func PrintList(head *Node<T>) {}
