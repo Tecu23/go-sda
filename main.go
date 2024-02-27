4a46e54aecf2d85c6ba59db3c406037f1d206b8f
Tecu23 <andrei.teculescu@gmail.com>
2024-02-27 11:59:45 +0200

    add stack implementation

--- main.go
+++ main.go
@@ -1,16 +1,39 @@
+// main
 package main
 
 import (
 	"fmt"
 
 	"github.com/Tecu23/go-sda/array"
+  "github.com/Tecu23/go-sda/stack"
 )
-
 func main() {
+  testStack()
+}
+
+func testQuickSort() {
 
 	tmp := []int{12, 11, 10, 9, 8, 7}
 
 	array.QuickSort(&tmp, 0, len(tmp)-1)
 
 	fmt.Println(tmp)
+}
+
+func testStack() {
+
+  stk := stack.New[int](10)
+
+  fmt.Print(stk)
+
+  fmt.Println(stk.IsEmpty())
+
+	tmp := []int{12, 11, 10, 9, 8, 7}
+  for _, s := range tmp {
+    stk.Push(&s)
+  }
+
+  fmt.Print(stk.Peek())
+
+  stk.Print()
 }
--- stack/stack.go
+++ stack/stack.go
@@ -1,24 +1,63 @@
+// Package stack implements a stack
 package stack
 
+import (
+	"errors"
+	"fmt"
+)
 
+// Stack is the structure of the Stack
 type Stack [T any] struct {
   arr []T
   top int
-  cap int
 }
 
+// New = Stack Constructor Function
 func New[T any](cap int) *Stack[T] {
-  arr := make([]T, cap)
-
-  stack := Stack[T]{top: 0, cap: cap, arr: arr}
+  arr := make([]T, 0, cap)
+  stack := Stack[T]{top: -1, arr: arr}
 
   return &stack
 }
 
-func (stack *Stack[T]) Len() int {return 0}
+// IsEmpty returns whether the stack is empty or not
+func (stack *Stack[T]) IsEmpty() bool {
+  return stack.top == -1
+}
+// Len retrieved the length of the stack
+func (stack *Stack[T]) Len() int {
+  return len(stack.arr) 
+}
 
-// func (stack *Stack[T]) Peek() T {}
-//
-// func (stack *Stack[T]) Pop() T {}
-//
-// func (stack *Stack[T]) Push(item T) {}
+// Peek retrieved the last item in the stack but does not remove it
+func (stack *Stack[T]) Peek() T {
+  return stack.arr[stack.top]
+}
+
+// Pop retrieves the last item and removes from the stack
+func (stack *Stack[T]) Pop() (*T, error) {
+
+  if stack.IsEmpty() {
+    return nil, errors.New("stack underflow! cannot pop element")
+  }
+
+  item := stack.arr[stack.top]
+  stack.top--
+
+  return &item, nil
+}
+
+// Push adds an itement to the stack
+func (stack *Stack[T]) Push(item *T) {
+
+  stack.arr = append(stack.arr, *item)
+  stack.top++
+}
+
+// Print displays the stack in order
+func (stack *Stack[T]) Print() {
+  
+  for _, v := range(stack.arr) {
+    fmt.Println(v)
+  }
+}
