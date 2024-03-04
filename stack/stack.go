cf24c77b790fc0b3fd6f7fb4d661d08a3e3c1a22
Tecu23 <andrei.teculescu@gmail.com>
2024-03-04 09:41:59 +0200

    add algorithms

--- stack/stack.go
+++ stack/stack.go
@@ -7,57 +7,55 @@ import (
 )
 
 // Stack is the structure of the Stack
-type Stack [T any] struct {
-  arr []T
-  top int
+type Stack[T any] struct {
+	arr []T
+	top int
 }
 
 // New = Stack Constructor Function
 func New[T any](cap int) *Stack[T] {
-  arr := make([]T, 0, cap)
-  stack := Stack[T]{top: -1, arr: arr}
+	arr := make([]T, 0, cap)
+	stack := Stack[T]{top: -1, arr: arr}
 
-  return &stack
+	return &stack
 }
 
 // IsEmpty returns whether the stack is empty or not
 func (stack *Stack[T]) IsEmpty() bool {
-  return stack.top == -1
+	return stack.top == -1
 }
+
 // Len retrieved the length of the stack
 func (stack *Stack[T]) Len() int {
-  return len(stack.arr) 
+	return len(stack.arr)
 }
 
 // Peek retrieved the last item in the stack but does not remove it
 func (stack *Stack[T]) Peek() T {
-  return stack.arr[stack.top]
+	return stack.arr[stack.top]
 }
 
 // Pop retrieves the last item and removes from the stack
 func (stack *Stack[T]) Pop() (*T, error) {
+	if stack.IsEmpty() {
+		return nil, errors.New("stack underflow! cannot pop element")
+	}
 
-  if stack.IsEmpty() {
-    return nil, errors.New("stack underflow! cannot pop element")
-  }
+	item := stack.arr[stack.top]
+	stack.top--
 
-  item := stack.arr[stack.top]
-  stack.top--
-
-  return &item, nil
+	return &item, nil
 }
 
 // Push adds an itement to the stack
 func (stack *Stack[T]) Push(item *T) {
-
-  stack.arr = append(stack.arr, *item)
-  stack.top++
+	stack.arr = append(stack.arr, *item)
+	stack.top++
 }
 
 // Print displays the stack in order
 func (stack *Stack[T]) Print() {
-  
-  for _, v := range(stack.arr) {
-    fmt.Println(v)
-  }
+	for _, v := range stack.arr {
+		fmt.Println(v)
+	}
 }
